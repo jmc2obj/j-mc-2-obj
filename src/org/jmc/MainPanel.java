570d6852b498f5381c70a4b35e82252ac880bc91
Maxovitsj@gmail.com <Maxovitsj@gmail.com@843485d3-f29e-551e-7a10-744002b1a3f5>
2012-02-23 22:10:34 +0000

    Changed: [MainPanel] 	- Set the LayoutManager to BorderLayout. (for now..) 	- Added scroll pane for the log. 	- Added a separate panel for the buttons. 	- Added a new 'preview' panel, for previewing the level.

--- src/org/jmc/MainPanel.java
+++ src/org/jmc/MainPanel.java
@@ -1,5 +1,7 @@
 package org.jmc;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -7,6 +9,7 @@ import java.io.File;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -15,16 +18,24 @@ public class MainPanel extends JPanel
 	private JButton load;
 	private JTextField path;
 	private JTextArea log;
+	private JScrollPane pane;
 	
 	public MainPanel()
 	{
+		setLayout(new BorderLayout());
+		JPanel buttons = new JPanel();
+		JPanel preview = new JPanel();
+		preview.setBackground(new Color(110,150,100));
 		load = new JButton("Load .dat");
 		path = new JTextField(20);
-		log = new JTextArea(20,70);
+		log = new JTextArea(5,1);
+		pane = new JScrollPane(log);
 		
-		add(path);
-		add(load);
-		add(log);
+		buttons.add(path);
+		buttons.add(load);
+		add(buttons, BorderLayout.NORTH);
+		add(preview);
+		add(pane, BorderLayout.SOUTH);
 		log.setLineWrap(true);
 		load.addActionListener(new ActionListener()
 		{
