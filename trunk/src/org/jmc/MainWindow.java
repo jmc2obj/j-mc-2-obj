744ae0c8467c069eb9316af51b3508ec406c6e99
danijel.korzinek@gmail.com <danijel.korzinek@gmail.com@843485d3-f29e-551e-7a10-744002b1a3f5>
2012-02-23 20:08:29 +0000

    Added empty window and project.

---
+++ trunk/src/org/jmc/MainWindow.java
@@ -0,0 +1,34 @@
+package org.jmc;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+@SuppressWarnings("serial")
+public class MainWindow extends JFrame{
+
+
+	MainWindow()
+	{
+		super("Main Window");
+
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+		setSize(640,480);
+
+		setVisible(true);
+	}
+
+	public static MainWindow main;
+
+	public static void main(String[] args) {
+
+		main=new MainWindow();
+
+	}
+
+}
