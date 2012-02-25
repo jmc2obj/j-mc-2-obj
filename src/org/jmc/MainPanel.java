7a2ad93aad3f200687a1877520b94df667f6a4a7
danijel.korzinek@gmail.com <danijel.korzinek@gmail.com@843485d3-f29e-551e-7a10-744002b1a3f5>
2012-02-25 21:49:06 +0000

    Reverted to before the image reducing changes.

--- src/org/jmc/MainPanel.java
+++ src/org/jmc/MainPanel.java
@@ -2,10 +2,13 @@ package org.jmc;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -13,6 +16,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.plaf.synth.Region;
 
 import org.jmc.NBT.TAG_Double;
 import org.jmc.NBT.TAG_List;
@@ -113,7 +117,7 @@ public class MainPanel extends JPanel
 						BufferedImage img=chunk.getBlocks();						
 						BufferedImage blend=preview.blend(img, height_img, 0.4);
 						
-						preview.addImage(blend, ix*16, iy*16);
+						preview.addImage(blend, ix*64, iy*64);
 						
 						preview.repaint();
 					
