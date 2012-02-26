d857bd860ac13212767b0397c2a70ec2bbed3983
danijel.korzinek@gmail.com <danijel.korzinek@gmail.com@843485d3-f29e-551e-7a10-744002b1a3f5>
2012-02-26 16:41:18 +0000

    Did some name refactoring wrt chunk loader threads. Added a chunk loader that is based on what is seen in the preview. Added facilities to allow reloading of maps (ie stopping loader threads).

---
+++ trunk/src/org/jmc/FullChunkLoaderThread.java
@@ -0,0 +1,83 @@
+package org.jmc;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.swing.JOptionPane;
+
+public class FullChunkLoaderThread implements ChunkLoaderThread {
+
+	private PreviewPanel preview;
+	private File savepath;
+	
+	private final int REPAINT_FREQUENCY=4;
+	
+	boolean running;
+	
+	public FullChunkLoaderThread(PreviewPanel preview, File savepath) {
+		this.preview=preview;
+		this.savepath=savepath;
+	}
+	
+	@Override
+	public void run() {
+	
+		running=true;
+		
+		Vector<AnvilRegion> regions=null;
+		try {
+			regions=AnvilRegion.loadAllRegions(savepath);
+		} catch (IOException e1) {
+			JOptionPane.showMessageDialog(null, "Couldn't load regions: "+e1);
+			return;
+		}				
+
+		int repaint_counter=REPAINT_FREQUENCY;
+		for(AnvilRegion region:regions)
+		{
+			for(Chunk chunk:region)
+			{											
+				if(chunk==null)
+				{
+					MainWindow.log("Chunk couldn't be loaded.");
+					return;
+				}
+
+				BufferedImage height_img=chunk.getHeightImage();
+				BufferedImage img=chunk.getBlocks();						
+				BufferedImage blend=preview.blend(img, height_img, 0.4);
+
+				int ix=chunk.getPosX();
+				int iy=chunk.getPosZ();
+				
+				preview.addImage(blend, ix*64, iy*64);
+				
+				repaint_counter--;
+				if(repaint_counter<=0)
+				{
+					preview.repaint();
+					repaint_counter=REPAINT_FREQUENCY;
+				}
+				
+				if(!running) return;
+			}
+
+		}
+		
+		running=false;
+		
+	}
+
+	@Override
+	public boolean isRunning() {
+		return running;
+	}
+
+	@Override
+	public void stopRunning() {
+		running=false;		
+	}
+
+}
---
+++ trunk/src/org/jmc/ViewChunkLoaderThread.java
@@ -0,0 +1,157 @@
+package org.jmc;
+
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Vector;
+
+import org.jmc.PreviewPanel.ChunkImage;
+
+public class ViewChunkLoaderThread implements ChunkLoaderThread {
+
+	private boolean running;
+	
+	private PreviewPanel preview;
+	private File savepath;
+	private Vector<ChunkImage> chunk_images;
+
+	private final int REPAINT_FREQUENCY=4;
+
+	public final int MAX_CHUNK_NUM=32768;
+
+	Set<Integer> loaded_chunks;
+
+	public ViewChunkLoaderThread(PreviewPanel preview, File savepath) {
+		this.preview=preview;
+		this.savepath=savepath;
+
+		chunk_images=preview.getChunkImages();
+
+		loaded_chunks=new HashSet<Integer>();
+	}
+
+	@Override
+	public void run() {		
+
+		running=true;
+		
+		AnvilRegion region=null;
+		Chunk chunk=null;
+
+		Rectangle prev_bounds=new Rectangle();
+
+		loaded_chunks.clear();
+
+		int repaint_counter=REPAINT_FREQUENCY;
+		while(running)
+		{
+			Rectangle bounds=preview.getChunkBounds();
+
+			boolean stop_iter=false;
+			if(!bounds.equals(prev_bounds))
+			{
+
+				int cxs=bounds.x;
+				int czs=bounds.y;
+				int cxe=bounds.x+bounds.width;
+				int cze=bounds.y+bounds.height;
+
+
+				Iterator<ChunkImage> iter=chunk_images.iterator();
+				while(iter.hasNext())
+				{
+					ChunkImage chunk_image=iter.next();
+					
+					int cx=chunk_image.x/64;
+					int cz=chunk_image.y/64;
+					
+					if(cx<cxs || cx>cxe || cz<czs || cz>cze)
+					{
+						loaded_chunks.remove(cx*MAX_CHUNK_NUM+cz);
+						iter.remove();
+					}
+					
+					Rectangle new_bounds=preview.getChunkBounds();
+					if(!bounds.equals(new_bounds))
+					{
+						stop_iter=true;
+						break;
+					}
+					
+					if(!running) return;
+				}		
+				
+				preview.redraw();
+
+				for(int cx=cxs; cx<=cxe && !stop_iter; cx++)
+				{
+					for(int cz=czs; cz<=cze && !stop_iter; cz++)
+					{					
+
+						if(loaded_chunks.contains(cx*MAX_CHUNK_NUM+cz)) continue;
+
+						loaded_chunks.add(cx*MAX_CHUNK_NUM+cz);
+
+						try {
+							region=AnvilRegion.findRegion(savepath, cx, cz);
+							chunk=region.getChunk(cx, cz);
+						} catch (Exception e) {
+							continue;
+						}
+
+						if(chunk==null) continue;
+
+						int ix=chunk.getPosX();
+						int iy=chunk.getPosZ();
+
+						BufferedImage height_img=chunk.getHeightImage();
+						BufferedImage img=chunk.getBlocks();						
+						BufferedImage blend=preview.blend(img, height_img, 0.4);					
+
+						preview.addImage(blend, ix*64, iy*64);
+
+						repaint_counter--;
+						if(repaint_counter<=0)
+						{
+							preview.repaint();
+							repaint_counter=REPAINT_FREQUENCY;
+						}
+
+						Rectangle new_bounds=preview.getChunkBounds();
+						if(!bounds.equals(new_bounds))
+							stop_iter=true;
+						
+						if(!running) return;
+					}
+				}			
+
+				preview.repaint();
+			}
+
+			prev_bounds=bounds;
+
+			if(!stop_iter)
+			{
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {}
+			}
+		}
+
+	}
+
+	@Override
+	public boolean isRunning() {
+		return running;
+	}
+
+	@Override
+	public void stopRunning() {
+		running=false;
+		
+	}
+
+}
