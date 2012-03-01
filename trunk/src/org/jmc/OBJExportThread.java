22f46df15b07b20f1575e3689d36085bcf1d4cc3
danijel.korzinek@gmail.com <danijel.korzinek@gmail.com@843485d3-f29e-551e-7a10-744002b1a3f5>
2012-03-01 11:38:51 +0000

    Fixed a small bug that happens when selection includes non-existing chunks.

--- trunk/src/org/jmc/OBJExportThread.java
+++ trunk/src/org/jmc/OBJExportThread.java
@@ -13,29 +13,29 @@ public class OBJExportThread extends JFrame implements Runnable {
 	File savepath;
 	Rectangle bounds;
 	int ymin;
-	
+
 	JProgressBar progress;
-	
+
 	public OBJExportThread(File savepath, Rectangle bounds, int ymin) 
 	{		
 		super("Export in progress...");		
-		
+
 		this.savepath=savepath;
 		this.bounds=bounds;
 		this.ymin=ymin;
-		
+
 		setSize(200,50);
-		
+
 		progress=new JProgressBar();
 		progress.setStringPainted(true);
 		add(progress);
 	}
-	
+
 	@Override
 	public void run() {
-		
+
 		setVisible(true);
-		
+
 		try {
 			FileWriter file=new FileWriter("out.obj");
 			PrintWriter writer=new PrintWriter(file);
@@ -45,14 +45,14 @@ public class OBJExportThread extends JFrame implements Runnable {
 			mtl.saveMTLFile(new File("minecraft.mtl"));
 
 			mtl.header(writer);
-			
+
 			int cxs=(int)Math.floor(bounds.x/16.0f);
 			int czs=(int)Math.floor(bounds.y/16.0f);
 			int cxe=(int)Math.ceil((bounds.x+bounds.width)/16.0f);
 			int cze=(int)Math.ceil((bounds.y+bounds.height)/16.0f);
 			int oxs=(cxe-cxs)/-2;
 			int ozs=(cze-czs)/-2;
-			
+
 			progress.setMaximum((cxe-cxs)*(cze-czs));
 
 			int progress_count=0;
@@ -60,8 +60,16 @@ public class OBJExportThread extends JFrame implements Runnable {
 				for(int cz=czs,oz=ozs; cz<=cze; cz++,oz++,progress_count++)
 				{
 					progress.setValue(progress_count);
-					Region region=Region.findRegion(savepath, cx, cz);					
-					Chunk chunk=region.getChunk(cx, cz);
+
+					Chunk chunk=null;
+					try{
+						Region region=Region.findRegion(savepath, cx, cz);							
+						if(region==null) continue;					
+						chunk=region.getChunk(cx, cz);
+						if(chunk==null) continue;
+					}catch (Exception e) {
+						continue;
+					}
 
 					OBJFile obj=chunk.getOBJ(mtl,bounds,ymin);
 
@@ -77,7 +85,7 @@ public class OBJExportThread extends JFrame implements Runnable {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		setVisible(false);
 
 	}
