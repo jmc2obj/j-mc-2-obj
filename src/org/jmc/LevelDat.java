ae1092dec7f011f247feb38017ae672e16788888
danijel.korzinek@gmail.com <danijel.korzinek@gmail.com@843485d3-f29e-551e-7a10-744002b1a3f5>
2012-03-06 08:53:47 +0000

    Added a copyright clause to all the files in the source.
    Started a javadoc.

--- src/org/jmc/LevelDat.java
+++ src/org/jmc/LevelDat.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2012
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ ******************************************************************************/
 package org.jmc;
 
 import java.io.File;
--- src/org/jmc/MainWindow.java
+++ src/org/jmc/MainWindow.java
@@ -1,12 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2012
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ ******************************************************************************/
 package org.jmc;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
+/**
+ * MainWindow class.
+ * 
+ * Used to start the program and set up initial program behavior.  
+ * 
+ * @author danijel
+ *
+ */
 @SuppressWarnings("serial")
 public class MainWindow extends JFrame
 {
+	/**
+	 * Panel containing the window's content.
+	 */
 	private MainPanel panel;
+	
+	/**
+	 * Window contructor.
+	 */
 	MainWindow()
 	{
 		super("Main Window");
@@ -30,12 +52,28 @@ public class MainWindow extends JFrame
 		setVisible(true);
 		
 	}
+	
+	/**
+	 * Global main window reference. 
+	 */
 	public static MainWindow main=null;
+	
+	
+	/**
+	 * Start of program.
+	 * 
+	 * @param args program arguments (currently unused)
+	 */
 	public static void main(String[] args)
 	{
 		main = new MainWindow();
 	}
 	
+	/**
+	 * Global logging method called by using MainWindow.log("test")
+	 * 
+	 * @param msg string to be logged
+	 */
 	public static void log(String msg)
 	{
 		if(main!=null) main.panel.log(msg);
