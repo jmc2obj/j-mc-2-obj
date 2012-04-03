d9cfc9510594f8b2d55438ac2305da5259452266
paol@draic.org <paol@draic.org@843485d3-f29e-551e-7a10-744002b1a3f5>
2012-04-03 15:09:35 +0000

    Texture exporter now supports texture packs and high-res textures.

--- trunk/src/org/jmc/Texsplit.java
+++ trunk/src/org/jmc/Texsplit.java
@@ -1,143 +1,47 @@
 package org.jmc;
 
-import java.awt.AlphaComposite;
 import java.awt.Color;
-import java.awt.Composite;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.jar.JarEntry;
-import java.util.jar.JarInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import javax.imageio.ImageIO;
-import javax.swing.JOptionPane;
 
+
+/**
+ * Utility class that can extract the individual textures from minecraft texture packs.  
+ */
 public class Texsplit
 {
-	final int width = 16;
-	final int height = 16;
-	final int rows = 16;
-	final int cols = 16;
+	final static int ROWS = 16;
+	final static int COLS = 16;
 	
-	public File terrain_file;
-	
-	public Texsplit(File terrain_file)
+
+	private static BufferedImage loadImageFromZip(File zipfile, String imagePath) throws IOException
 	{
-		this.terrain_file=terrain_file;
-	}
-	
-	public Texsplit()
-	{
-		this.terrain_file=null;
-	}
-	
-	
-	private BufferedImage loadImageFromFile(File file)
-	{
-		try
+		ZipInputStream zis = new ZipInputStream(new FileInputStream(zipfile));
+
+		ZipEntry entry = null;
+		while ((entry = zis.getNextEntry()) != null)
 		{
-			return ImageIO.read(terrain_file);			
-		} 
-		catch (IOException e)
-		{
-			JOptionPane.showMessageDialog(MainWindow.main, "Cannot open terrain file:\n"+e.getMessage());
-			return null;
+			if (!entry.isDirectory() && entry.getName().equals(imagePath))
+				break;
 		}
+
+		if (entry == null)
+			throw new IOException("Couldn't find " + imagePath + " in " + zipfile.getName());
+
+		BufferedImage result = ImageIO.read(zis);
+		zis.close();
+
+		return result;
 	}
 	
-	private BufferedImage loadImageFromMinecraft()
-	{
-		File minecraft_jar=new File(MainPanel.getMinecraftDir()+"/bin/minecraft.jar");
-		if(!minecraft_jar.canRead())
-		{
-			JOptionPane.showMessageDialog(MainWindow.main, "Cannot open minecraft.jar file. Are you sure you have minecraft installed?");
-			return null;
-		}
-				
-		try {								
-			JarInputStream jis=new JarInputStream(new FileInputStream(minecraft_jar));
-			
-			JarEntry entry=null;
-			while((entry=jis.getNextJarEntry())!=null)
-			{
-				if(!entry.isDirectory() && entry.getName().equals("terrain.png"))
-					break;
-			}
-			
-			if(entry==null)
-			{
-				MainWindow.log("Couldn't find terrain.png in minecraft.jar!");
-				return null;
-			}
-			
-			return ImageIO.read(jis);
-						
-			
-		} catch (IOException e) {
-			MainWindow.log("Error reading minecraft jar file: "+e.getMessage());
-			return null;
-		}
-	}
-	
-	public void splitTextures(File destination, boolean save_alpha)
-	{
-	
-		if(destination==null)
-			return;
-		
-		BufferedImage texture=null;
-		if(terrain_file!=null)
-			texture=loadImageFromFile(terrain_file);
-		else 
-			texture=loadImageFromMinecraft();
-			
-		if(texture==null) return;
-	
-		BufferedImage[] textures = new BufferedImage[rows * cols];
-		int counter = 0;
-		for(int i = 0; i < rows; i++)
-		{
-			for(int j = 0; j < cols; j++)
-			{
-				textures[(i * cols) + j] = texture.getSubimage(j * width, i * height, width, height);
-				BufferedImage im = textures[(i * cols) + j];
-				counter++;
-				try
-				{					
-					if (counter < rows*cols)
-					{										
-						if(counter==1 || counter==53 || counter==40 || counter==144)
-						{
-							tintImage(im, Color.green);
-						}
-						File f=new File(destination.getAbsolutePath() + "/" + counter + ".png");
-						ImageIO.write(im, "png", f);				
-						MainWindow.log("Saving texture to: "+f.getAbsolutePath());
-						
-						if(save_alpha)
-						{
-							convertToAlpha(im);
-							File f2=new File(destination.getAbsolutePath() + "/" + counter + "_a.png");
-							ImageIO.write(im, "png", f2);				
-							MainWindow.log("Saving texture alpha to: "+f2.getAbsolutePath());
-						}
-					}
-				} 
-				catch (IOException e)
-				{
-					JOptionPane.showMessageDialog(MainWindow.main, "Cannot save textures:\n"+e.getMessage());
-					return;
-				}
-			}
-		}
-	}
-	
-	private int buffer[]=null;
-	
-	private void convertToAlpha(BufferedImage img)
+	private static void convertToAlpha(BufferedImage img)
 	{
 		int w=img.getWidth();
 		int h=img.getHeight();
@@ -149,9 +53,8 @@ public class Texsplit
 			return;
 		}
 		
-		if(buffer==null || buffer.length<w*h*c)
-			buffer=new int[w*h*c];
-		
+		int buffer[]=new int[w*h*c];
+
 		WritableRaster raster=img.getRaster();
 		raster.getPixels(0, 0, w, h, buffer);
 		
@@ -166,7 +69,7 @@ public class Texsplit
 		raster.setPixels(0, 0, w, h, buffer);
 	}
 	
-	private void tintImage(BufferedImage img, Color tint)
+	private static void tintImage(BufferedImage img, Color tint)
 	{
 		int w=img.getWidth();
 		int h=img.getHeight();
@@ -178,9 +81,8 @@ public class Texsplit
 			return;
 		}
 		
-		if(buffer==null || buffer.length<w*h*c)
-			buffer=new int[w*h*c];
-		
+		int buffer[]=new int[w*h*c];
+
 		WritableRaster raster=img.getRaster();
 		raster.getPixels(0, 0, w, h, buffer);
 		
@@ -205,4 +107,60 @@ public class Texsplit
 		
 		raster.setPixels(0, 0, w, h, buffer);
 	}
+
+
+	/**
+	 * Reads a Minecraft texture pack and splits the individual block textures into .png images.
+	 * 
+	 * @param destination Directory to place the output files.
+	 * @param texturePack A Minecraft texture pack file. If null, will use minecraft's default textures.
+	 */
+	public static void splitTextures(File destination, File texturePack) throws Exception
+	{
+		if(destination==null)
+			throw new IllegalArgumentException("destination cannot be null");
+	
+		File zipfile;
+		if (texturePack == null)
+			zipfile = new File(Utility.getMinecraftDir(), "bin/minecraft.jar");
+		else
+			zipfile = texturePack;
+
+		if (!zipfile.canRead())
+			throw new Exception("Cannot open " + zipfile.getName());
+		
+		BufferedImage image = loadImageFromZip(zipfile, "terrain.png");
+
+		int width = image.getWidth() / COLS;
+		int height = image.getHeight() / ROWS;
+		BufferedImage[] textures = new BufferedImage[ROWS * COLS];
+		
+		int counter = 0;
+		for(int i = 0; i < ROWS; i++)
+		{
+			for(int j = 0; j < COLS; j++)
+			{
+				textures[(i * COLS) + j] = image.getSubimage(j * width, i * height, width, height);
+				BufferedImage im = textures[(i * COLS) + j];
+				counter++;
+
+				if (counter < ROWS*COLS)
+				{
+					if(counter==1 || counter==53 || counter==40 || counter==144)
+					{
+						tintImage(im, Color.green);
+					}
+					File f=new File(destination.getAbsolutePath() + "/" + counter + ".png");
+					ImageIO.write(im, "png", f);
+					MainWindow.log("Saving texture to: "+f.getAbsolutePath());
+					
+					convertToAlpha(im);
+					File f2=new File(destination.getAbsolutePath() + "/" + counter + "_a.png");
+					ImageIO.write(im, "png", f2);				
+					MainWindow.log("Saving texture alpha to: "+f2.getAbsolutePath());
+				}
+			}
+		}
+	}
+	
 }
