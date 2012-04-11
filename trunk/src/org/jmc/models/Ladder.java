90d5e5ab82661fa574eba7fc1af9887c2b6d6bdd
paol@draic.org <paol@draic.org@843485d3-f29e-551e-7a10-744002b1a3f5>
2012-04-11 15:16:04 +0000

    Implemented Doors, Ladders

---
+++ trunk/src/org/jmc/models/Ladder.java
@@ -0,0 +1,47 @@
+package org.jmc.models;
+
+import org.jmc.ChunkDataBuffer;
+import org.jmc.OBJFile;
+import org.jmc.OBJFile.Side;
+import org.jmc.Transform;
+import org.jmc.Vertex;
+
+
+/**
+ * Model for ladders.
+ */
+public class Ladder extends BlockModel
+{
+
+	@Override
+	public void addModel(OBJFile obj, ChunkDataBuffer chunks, int x, int y, int z, byte data)
+	{
+		Transform rotate = new Transform();
+		Transform translate = new Transform();
+		Transform rt;
+		
+		switch (data)
+		{
+			case 2:
+				rotate.rotate(0, 180, 0);
+				break;
+			case 4:
+				rotate.rotate(0, 90, 0);
+				break;
+			case 5:
+				rotate.rotate(0, -90, 0);
+				break;
+		}
+		translate.translate(x, y, z);		
+			
+		rt = translate.multiply(rotate);
+		
+		Vertex[] vertices = new Vertex[4];
+		vertices[0] = new Vertex( 0.5f, -0.5f, -0.49f);
+		vertices[1] = new Vertex( 0.5f,  0.5f, -0.49f);			
+		vertices[2] = new Vertex(-0.5f,  0.5f, -0.49f);
+		vertices[3] = new Vertex(-0.5f, -0.5f, -0.49f);
+		obj.addFace(vertices, rt, Side.FRONT, materials.get(data)[0]);
+	}
+
+}
