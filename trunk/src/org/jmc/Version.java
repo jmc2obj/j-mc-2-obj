b98059f1c58e1d4888cb15ecb96f04aea1097292
paol@draic.org <paol@draic.org@843485d3-f29e-551e-7a10-744002b1a3f5>
2012-05-15 22:30:00 +0000

    Implemented Chest

--- trunk/src/org/jmc/Version.java
+++ trunk/src/org/jmc/Version.java
@@ -7,5 +7,5 @@ package org.jmc;
 public class Version
 {
 	public static final String VERSION = "0.15-dev";
-	public static final String REVISION = "r187";
+	public static final String REVISION = "r201";
 }
