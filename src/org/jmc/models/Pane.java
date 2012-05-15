cadf360ea35bb197c2a12f1fcb995c0ac38839e7
paol@draic.org <paol@draic.org@843485d3-f29e-551e-7a10-744002b1a3f5>
2012-05-15 18:17:48 +0000

    Improved Pane model. Added "object per chunk" option to command line mode.

--- src/org/jmc/models/Pane.java
+++ src/org/jmc/models/Pane.java
@@ -11,8 +11,6 @@ import org.jmc.geom.Vertex;
 
 /**
  * Model for glass panes and iron bars.
- * 
- * TODO add pane borders
  */
 public class Pane extends BlockModel
 {
@@ -31,11 +29,17 @@ public class Pane extends BlockModel
 	@Override
 	public void addModel(OBJOutputFile obj, ChunkDataBuffer chunks, int x, int y, int z, byte data)
 	{
+		String mtl = materials.get(data)[0];
+		String mtlSide = materials.get(data)[1];
+		
 		boolean n = checkConnect(chunks.getBlockID(x, y, z-1));
 		boolean s = checkConnect(chunks.getBlockID(x, y, z+1));
 		boolean e = checkConnect(chunks.getBlockID(x+1, y, z));
 		boolean w = checkConnect(chunks.getBlockID(x-1, y, z));
 		boolean none = !(n || s || e || w);
+
+		boolean up = checkConnect(chunks.getBlockID(x, y+1, z));
+		boolean down = checkConnect(chunks.getBlockID(x, y-1, z));
 		
 		Transform t = new Transform();
 		t.translate(x, y, z);		
@@ -44,37 +48,187 @@ public class Pane extends BlockModel
 		UV[] uv = new UV[4];
 		if (n || none)
 		{
-			vertices[0] = new Vertex(0.0f, -0.5f, -0.5f); uv[0] = new UV(0, 0);
-			vertices[1] = new Vertex(0.0f, -0.5f,  0.0f); uv[1] = new UV(0.5f, 0);
-			vertices[2] = new Vertex(0.0f,  0.5f,  0.0f); uv[2] = new UV(0.5f, 1);
-			vertices[3] = new Vertex(0.0f,  0.5f, -0.5f); uv[3] = new UV(0, 1);
-			obj.addFace(vertices, uv, t, materials.get(data)[0]);
+			uv[0] = new UV(0, 0);
+			uv[1] = new UV(0.5f, 0);
+			uv[2] = new UV(0.5f, 1);
+			uv[3] = new UV(0, 1);
+			vertices[0] = new Vertex(0.0f, -0.5f, -0.5f);
+			vertices[1] = new Vertex(0.0f, -0.5f,  0.0f);
+			vertices[2] = new Vertex(0.0f,  0.5f,  0.0f);
+			vertices[3] = new Vertex(0.0f,  0.5f, -0.5f);
+			obj.addFace(vertices, uv, t, mtl);
+
+			uv[0] = new UV(7/16f, 0.5f);
+			uv[1] = new UV(9/16f, 0.5f);
+			uv[2] = new UV(9/16f, 1);
+			uv[3] = new UV(7/16f, 1);
+			if (!up)
+			{
+				vertices[0] = new Vertex(-1/16f, 0.5f, 0);
+				vertices[1] = new Vertex( 1/16f, 0.5f, 0);
+				vertices[2] = new Vertex( 1/16f, 0.5f, -0.5f);
+				vertices[3] = new Vertex(-1/16f, 0.5f, -0.5f);
+				obj.addFace(vertices, uv, t, mtlSide);
+			}
+			if (!down)
+			{
+				vertices[0] = new Vertex( 1/16f, -0.5f, 0);
+				vertices[1] = new Vertex(-1/16f, -0.5f, 0);
+				vertices[2] = new Vertex(-1/16f, -0.5f, -0.5f);
+				vertices[3] = new Vertex( 1/16f, -0.5f, -0.5f);
+				obj.addFace(vertices, uv, t, mtlSide);
+			}
 		}
-		if (s || none)
+		else if (s && !e && !w)
 		{
-			vertices[0] = new Vertex(0.0f, -0.5f, 0.0f); uv[0] = new UV(0.5f, 0);
-			vertices[1] = new Vertex(0.0f, -0.5f, 0.5f); uv[1] = new UV(1, 0);
-			vertices[2] = new Vertex(0.0f,  0.5f, 0.5f); uv[2] = new UV(1, 1);
-			vertices[3] = new Vertex(0.0f,  0.5f, 0.0f); uv[3] = new UV(0.5f, 1);
-			obj.addFace(vertices, uv, t, materials.get(data)[0]);
-		}
-		if (e || none)
-		{
-			vertices[0] = new Vertex(0.5f, -0.5f, 0.0f); uv[0] = new UV(0, 0);
-			vertices[1] = new Vertex(0.0f, -0.5f, 0.0f); uv[1] = new UV(0.5f, 0);
-			vertices[2] = new Vertex(0.0f,  0.5f, 0.0f); uv[2] = new UV(0.5f, 1);
-			vertices[3] = new Vertex(0.5f,  0.5f, 0.0f); uv[3] = new UV(0, 1);
-			obj.addFace(vertices, uv, t, materials.get(data)[0]);
-		}
-		if (w || none)
-		{
-			vertices[0] = new Vertex( 0.0f, -0.5f, 0.0f); uv[0] = new UV(0.5f, 0);
-			vertices[1] = new Vertex(-0.5f, -0.5f, 0.0f); uv[1] = new UV(1, 0);
-			vertices[2] = new Vertex(-0.5f,  0.5f, 0.0f); uv[2] = new UV(1, 1);
-			vertices[3] = new Vertex( 0.0f,  0.5f, 0.0f); uv[3] = new UV(0.5f, 1);
-			obj.addFace(vertices, uv, t, materials.get(data)[0]);
+			uv[0] = new UV(7/16f, 0);
+			uv[1] = new UV(9/16f, 0);
+			uv[2] = new UV(9/16f, 1);
+			uv[3] = new UV(7/16f, 1);
+			vertices[0] = new Vertex( 1/16f, -0.5f, 0);
+			vertices[1] = new Vertex(-1/16f, -0.5f, 0);
+			vertices[2] = new Vertex(-1/16f,  0.5f, 0);
+			vertices[3] = new Vertex( 1/16f,  0.5f, 0);
+			obj.addFace(vertices, uv, t, mtlSide);
 		}
 		
+		if (s || none)
+		{
+			uv[0] = new UV(0.5f, 0);
+			uv[1] = new UV(1, 0);
+			uv[2] = new UV(1, 1);
+			uv[3] = new UV(0.5f, 1);
+			vertices[0] = new Vertex(0.0f, -0.5f, 0.0f);
+			vertices[1] = new Vertex(0.0f, -0.5f, 0.5f);
+			vertices[2] = new Vertex(0.0f,  0.5f, 0.5f);
+			vertices[3] = new Vertex(0.0f,  0.5f, 0.0f);
+			obj.addFace(vertices, uv, t, mtl);
+
+			uv[0] = new UV(7/16f, 0);
+			uv[1] = new UV(9/16f, 0);
+			uv[2] = new UV(9/16f, 0.5f);
+			uv[3] = new UV(7/16f, 0.5f);
+			if (!up)
+			{
+				vertices[0] = new Vertex(-1/16f, 0.5f, 0.5f);
+				vertices[1] = new Vertex( 1/16f, 0.5f, 0.5f);
+				vertices[2] = new Vertex( 1/16f, 0.5f, 0);
+				vertices[3] = new Vertex(-1/16f, 0.5f, 0);
+				obj.addFace(vertices, uv, t, mtlSide);
+			}
+			if (!down)
+			{
+				vertices[0] = new Vertex( 1/16f, -0.5f, 0.5f);
+				vertices[1] = new Vertex(-1/16f, -0.5f, 0.5f);
+				vertices[2] = new Vertex(-1/16f, -0.5f, 0);
+				vertices[3] = new Vertex( 1/16f, -0.5f, 0);
+				obj.addFace(vertices, uv, t, mtlSide);
+			}
+		}
+		else if (n && !e && !w)
+		{
+			uv[0] = new UV(7/16f, 0);
+			uv[1] = new UV(9/16f, 0);
+			uv[2] = new UV(9/16f, 1);
+			uv[3] = new UV(7/16f, 1);
+			vertices[0] = new Vertex(-1/16f, -0.5f, 0);
+			vertices[1] = new Vertex( 1/16f, -0.5f, 0);
+			vertices[2] = new Vertex( 1/16f,  0.5f, 0);
+			vertices[3] = new Vertex(-1/16f,  0.5f, 0);
+			obj.addFace(vertices, uv, t, mtlSide);
+		}
+
+		if (e || none)
+		{
+			uv[0] = new UV(0, 0);
+			uv[1] = new UV(0.5f, 0);
+			uv[2] = new UV(0.5f, 1);
+			uv[3] = new UV(0, 1);
+			vertices[0] = new Vertex(0.5f, -0.5f, 0.0f);
+			vertices[1] = new Vertex(0.0f, -0.5f, 0.0f);
+			vertices[2] = new Vertex(0.0f,  0.5f, 0.0f);
+			vertices[3] = new Vertex(0.5f,  0.5f, 0.0f);
+			obj.addFace(vertices, uv, t, mtl);
+
+			uv[0] = new UV(9/16f, 0.5f);
+			uv[1] = new UV(9/16f, 1);
+			uv[2] = new UV(7/16f, 1);
+			uv[3] = new UV(7/16f, 0.5f);
+			if (!up)
+			{
+				vertices[0] = new Vertex(0, 0.5f,  1/16f);
+				vertices[1] = new Vertex(0.5f, 0.5f,  1/16f);
+				vertices[2] = new Vertex(0.5f, 0.5f, -1/16f);
+				vertices[3] = new Vertex(0, 0.5f, -1/16f);
+				obj.addFace(vertices, uv, t, mtlSide);
+			}
+			if (!down)
+			{
+				vertices[0] = new Vertex(0.5f, -0.5f,  1/16f);
+				vertices[1] = new Vertex(0, -0.5f,  1/16f);
+				vertices[2] = new Vertex(0, -0.5f, -1/16f);
+				vertices[3] = new Vertex(0.5f, -0.5f, -1/16f);
+				obj.addFace(vertices, uv, t, mtlSide);
+			}
+		}
+		else if (w && !n && !s)
+		{
+			uv[0] = new UV(7/16f, 0);
+			uv[1] = new UV(9/16f, 0);
+			uv[2] = new UV(9/16f, 1);
+			uv[3] = new UV(7/16f, 1);
+			vertices[0] = new Vertex(0, -0.5f,  1/16f);
+			vertices[1] = new Vertex(0, -0.5f, -1/16f);
+			vertices[2] = new Vertex(0,  0.5f, -1/16f);
+			vertices[3] = new Vertex(0,  0.5f,  1/16f);
+			obj.addFace(vertices, uv, t, mtlSide);
+		}
+		
+		if (w || none)
+		{
+			uv[0] = new UV(0.5f, 0);
+			uv[1] = new UV(1, 0);
+			uv[2] = new UV(1, 1);
+			uv[3] = new UV(0.5f, 1);
+			vertices[0] = new Vertex( 0.0f, -0.5f, 0.0f);
+			vertices[1] = new Vertex(-0.5f, -0.5f, 0.0f);
+			vertices[2] = new Vertex(-0.5f,  0.5f, 0.0f);
+			vertices[3] = new Vertex( 0.0f,  0.5f, 0.0f);
+			obj.addFace(vertices, uv, t, mtl);
+
+			uv[0] = new UV(9/16f, 0);
+			uv[1] = new UV(9/16f, 0.5f);
+			uv[2] = new UV(7/16f, 0.5f);
+			uv[3] = new UV(7/16f, 0);
+			if (!up)
+			{
+				vertices[0] = new Vertex(-0.5f, 0.5f,  1/16f);
+				vertices[1] = new Vertex(0, 0.5f,  1/16f);
+				vertices[2] = new Vertex(0, 0.5f, -1/16f);
+				vertices[3] = new Vertex(-0.5f, 0.5f, -1/16f);
+				obj.addFace(vertices, uv, t, mtlSide);
+			}
+			if (!down)
+			{
+				vertices[0] = new Vertex(0, -0.5f,  1/16f);
+				vertices[1] = new Vertex(-0.5f, -0.5f,  1/16f);
+				vertices[2] = new Vertex(-0.5f, -0.5f, -1/16f);
+				vertices[3] = new Vertex(0, -0.5f, -1/16f);
+				obj.addFace(vertices, uv, t, mtlSide);
+			}
+		}
+		else if (e && !n && !s)
+		{
+			uv[0] = new UV(7/16f, 0);
+			uv[1] = new UV(9/16f, 0);
+			uv[2] = new UV(9/16f, 1);
+			uv[3] = new UV(7/16f, 1);
+			vertices[0] = new Vertex(0, -0.5f, -1/16f);
+			vertices[1] = new Vertex(0, -0.5f,  1/16f);
+			vertices[2] = new Vertex(0,  0.5f,  1/16f);
+			vertices[3] = new Vertex(0,  0.5f, -1/16f);
+			obj.addFace(vertices, uv, t, mtlSide);
+		}
 	}
 
 }
