855f7d1d75e06e2081a34407f6258a0bc7aa5c3c
paol@draic.org <paol@draic.org@843485d3-f29e-551e-7a10-744002b1a3f5>
2012-06-08 11:10:07 +0000

    Improved Fire model.

--- trunk/src/org/jmc/models/Fire.java
+++ trunk/src/org/jmc/models/Fire.java
@@ -2,6 +2,8 @@ package org.jmc.models;
 
 import org.jmc.ChunkDataBuffer;
 import org.jmc.OBJOutputFile;
+import org.jmc.geom.Transform;
+import org.jmc.geom.Vertex;
 
 
 /**
@@ -13,15 +15,64 @@ public class Fire extends BlockModel
 	@Override
 	public void addModel(OBJOutputFile obj, ChunkDataBuffer chunks, int x, int y, int z, byte data)
 	{
-		boolean[] drawSides = new boolean[] { false, true, true, true, true, false};
+		String[] mtlSides = getMtlSides(data);
+
+		Transform t = new Transform();
+		t.translate(x, y, z);
 		
-		addBox(obj,
-				x-0.49f, y-0.49f, z-0.49f,
-				x+0.49f, y+0.49f, z+0.49f, 
-				null, 
-				getMtlSides(data), 
-				null, 
-				drawSides);
+		Vertex[] vertices = new Vertex[4];
+		
+		// front
+		vertices[0] = new Vertex( 0.5f, -0.5f, -0.5f);
+		vertices[1] = new Vertex(-0.5f, -0.5f, -0.5f);
+		vertices[2] = new Vertex(-0.45f,  0.6f, -0.45f);
+		vertices[3] = new Vertex( 0.45f,  0.6f, -0.45f);
+		obj.addFace(vertices, null, t, mtlSides[1]);
+
+		vertices[0] = new Vertex( 0.49f, -0.5f, -0.25f);
+		vertices[1] = new Vertex(-0.49f, -0.5f, -0.25f);
+		vertices[2] = new Vertex(-0.44f,  0.75f, 0.25f);
+		vertices[3] = new Vertex( 0.44f,  0.75f, 0.25f);
+		obj.addFace(vertices, null, t, mtlSides[3]);
+
+		// back
+		vertices[0] = new Vertex(-0.5f, -0.5f,  0.5f);
+		vertices[1] = new Vertex( 0.5f, -0.5f,  0.5f);
+		vertices[2] = new Vertex( 0.45f,  0.6f,  0.45f);
+		vertices[3] = new Vertex(-0.45f,  0.6f,  0.45f);
+		obj.addFace(vertices, null, t, mtlSides[2]);
+
+		vertices[0] = new Vertex(-0.49f, -0.5f,  0.25f);
+		vertices[1] = new Vertex( 0.49f, -0.5f,  0.25f);
+		vertices[2] = new Vertex( 0.44f,  0.75f, -0.25f);
+		vertices[3] = new Vertex(-0.44f,  0.75f, -0.25f);
+		obj.addFace(vertices, null, t, mtlSides[4]);
+
+		// left
+		vertices[0] = new Vertex(-0.5f, -0.5f, -0.5f);
+		vertices[1] = new Vertex(-0.5f, -0.5f,  0.5f);
+		vertices[2] = new Vertex(-0.45f,  0.6f,  0.45f);
+		vertices[3] = new Vertex(-0.45f,  0.6f, -0.45f);
+		obj.addFace(vertices, null, t, mtlSides[3]);
+
+		vertices[0] = new Vertex(-0.25f, -0.5f, -0.49f);
+		vertices[1] = new Vertex(-0.25f, -0.5f,  0.49f);
+		vertices[2] = new Vertex( 0.25f,  0.75f,  0.44f);
+		vertices[3] = new Vertex( 0.25f,  0.75f, -0.44f);
+		obj.addFace(vertices, null, t, mtlSides[1]);
+
+		// right
+		vertices[0] = new Vertex( 0.5f, -0.5f,  0.5f);
+		vertices[1] = new Vertex( 0.5f, -0.5f, -0.5f);
+		vertices[2] = new Vertex( 0.45f,  0.6f, -0.45f);
+		vertices[3] = new Vertex( 0.45f,  0.6f,  0.45f);
+		obj.addFace(vertices, null, t, mtlSides[4]);
+
+		vertices[0] = new Vertex( 0.25f, -0.5f,  0.49f);
+		vertices[1] = new Vertex( 0.25f, -0.5f, -0.49f);
+		vertices[2] = new Vertex(-0.25f,  0.75f, -0.44f);
+		vertices[3] = new Vertex(-0.25f,  0.75f,  0.44f);
+		obj.addFace(vertices, null, t, mtlSides[2]);
 	}
 
 }
