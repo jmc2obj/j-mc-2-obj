83383fdf538fde70cfa70c22694401368a27e883
paol@draic.org <paol@draic.org@843485d3-f29e-551e-7a10-744002b1a3f5>
2013-12-04 20:48:04 +0000

    - Added new flowers: Red, Orange, White and Pink Tulips, Blue Orchid, Azure Bluet, Oxeye Daisies, Allium - Implemented 2 block tall plants: Sunflowers, Pink Peonies, Rosebush, Lilac, Double Tallgrass, Large Fern

---
+++ src/org/jmc/models/DoublePlant.java
@@ -0,0 +1,75 @@
+package org.jmc.models;
+
+import org.jmc.ChunkDataBuffer;
+import org.jmc.OBJOutputFile;
+import org.jmc.geom.Transform;
+import org.jmc.geom.Vertex;
+
+
+/**
+ * Model for 2 block tall plants.
+ */
+public class DoublePlant extends BlockModel
+{
+
+	@Override
+	public void addModel(OBJOutputFile obj, ChunkDataBuffer chunks, int x, int y, int z, byte data, byte biome)
+	{
+		boolean top = (data & 8) != 0;
+		if (top) {
+			// must get the type of plant from the block below
+			data = chunks.getBlockData(x, y-1, z);
+		}
+
+		String[] mtls = materials.get(data, biome);
+
+		Transform t = new Transform();
+		t.translate(x, y, z);
+		
+		if (top) {
+			Vertex[] vertices = new Vertex[4];
+			vertices[0] = new Vertex(+0.5f,-0.5f,-0.5f);
+			vertices[1] = new Vertex(-0.5f,-0.5f,+0.5f);
+			vertices[2] = new Vertex(-0.5f,+0.5f,+0.5f);
+			vertices[3] = new Vertex(+0.5f,+0.5f,-0.5f);
+			obj.addFace(vertices, null, t, mtls[1]);
+			
+			vertices[0] = new Vertex(-0.5f,-0.5f,-0.5f);
+			vertices[1] = new Vertex(+0.5f,-0.5f,+0.5f);
+			vertices[2] = new Vertex(+0.5f,+0.5f,+0.5f);
+			vertices[3] = new Vertex(-0.5f,+0.5f,-0.5f);
+			obj.addFace(vertices, null, t, mtls[1]);
+
+			if (data == 0) {
+				// Sunflower
+				Transform r = new Transform();
+				Double o = (double)(x*y*z);
+				r.rotate(0, (o.hashCode() % 30) - 15, 15);
+				
+				Transform rt;
+				rt = t.multiply(r);
+
+				vertices[0] = new Vertex(0.125f,-0.375f,+0.5f);
+				vertices[1] = new Vertex(0.125f,-0.375f,-0.5f);
+				vertices[2] = new Vertex(0.125f,+0.625f,-0.5f);
+				vertices[3] = new Vertex(0.125f,+0.625f,+0.5f);
+				obj.addFace(vertices, null, rt, mtls[3]);
+			}
+		}
+		else {
+			Vertex[] vertices = new Vertex[4];
+			vertices[0] = new Vertex(+0.5f,-0.5f,-0.5f);
+			vertices[1] = new Vertex(-0.5f,-0.5f,+0.5f);
+			vertices[2] = new Vertex(-0.5f,+0.5f,+0.5f);
+			vertices[3] = new Vertex(+0.5f,+0.5f,-0.5f);
+			obj.addFace(vertices, null, t, mtls[0]);
+			
+			vertices[0] = new Vertex(-0.5f,-0.5f,-0.5f);
+			vertices[1] = new Vertex(+0.5f,-0.5f,+0.5f);
+			vertices[2] = new Vertex(+0.5f,+0.5f,+0.5f);
+			vertices[3] = new Vertex(-0.5f,+0.5f,-0.5f);
+			obj.addFace(vertices, null, t, mtls[0]);
+		}
+	}
+
+}
