d024539f45b6cffc3c00af8b2d06e5f7fff6e52b
paol@draic.org <paol@draic.org@843485d3-f29e-551e-7a10-744002b1a3f5>
2015-04-18 13:52:55 +0000

    Fix Painting entity (apparently the painting NBT tags must have changed, as a result we were getting an NMPE)

--- src/org/jmc/entities/Painting.java
+++ src/org/jmc/entities/Painting.java
@@ -8,39 +8,76 @@ import org.jmc.NBT.TAG_Int;
 import org.jmc.NBT.TAG_String;
 import org.jmc.geom.Transform;
 
-public class Painting extends Entity {
 
+/**
+ * Model for paintings.
+ * TODO correctly render paintings larger than 1x1
+ */
+public class Painting extends Entity
+{
 	@Override
-	public void addEntity(OBJOutputFile obj, TAG_Compound entity) {
+	public void addEntity(OBJOutputFile obj, TAG_Compound entity)
+	{
+		/*
+		Example of a painting entity as of MC 1.8:
+		
+		TAG_Compound(""): count=18
+			TAG_Byte("Facing"): dec=1
+			TAG_Long("UUIDLeast"): val=-6055910250618088878
+			TAG_List("Motion"): count=3 type=6
+				TAG_Double(""): val=0.0
+				TAG_Double(""): val=0.0
+				TAG_Double(""): val=0.0
+			ENDOF TAG_List("Motion")
+			TAG_Byte("OnGround"): dec=0
+			TAG_Long("UUIDMost"): val=-8729761873231721078
+			TAG_Int("TileY"): val=102
+			TAG_Int("Dimension"): val=0
+			TAG_Int("TileX"): val=-1265
+			TAG_Short("Air"): val=300
+			TAG_Int("TileZ"): val=-3932
+			TAG_String("Motive"): val=Match
+			TAG_String("id"): val=Painting
+			TAG_List("Pos"): count=3 type=6
+				TAG_Double(""): val=-1264.03125
+				TAG_Double(""): val=103.0
+				TAG_Double(""): val=-3931.0
+			ENDOF TAG_List("Pos")
+			TAG_Int("PortalCooldown"): val=0
+			TAG_Short("Fire"): val=0
+			TAG_Float("FallDistance"): val=0.0
+			TAG_List("Rotation"): count=2 type=5
+				TAG_Float(""): val=90.0
+				TAG_Float(""): val=0.0
+			ENDOF TAG_List("Rotation")
+			TAG_Byte("Invulnerable"): dec=0
+		ENDOF TAG_Compound("")
+		*/
 		
 		int x=((TAG_Int)entity.getElement("TileX")).value;
 		int y=((TAG_Int)entity.getElement("TileY")).value;
 		int z=((TAG_Int)entity.getElement("TileZ")).value;
 		
-		byte dir=((TAG_Byte)entity.getElement("Dir")).value;
-		
-		String motiv=((TAG_String)entity.getElement("Motive")).value;
+		byte facing = ((TAG_Byte)entity.getElement("Facing")).value;
 
+		String motiv=((TAG_String)entity.getElement("Motive")).value;
+		
 		Transform rotate = new Transform();
 		Transform translate = new Transform();
 		Transform rt;
 		
-		switch (dir)
+		switch (facing)
 		{
 			case 0:
-				z--;
-				rotate.rotate(0, 180, 0);
 				break;
 			case 1:
-				x--;
-				rotate.rotate(0, 90, 0);				
+				rotate.rotate(0, 90, 0);
 				break;
 			case 2:
-				z++;
-				break;				
+				rotate.rotate(0, 180, 0);
+				break;
 			case 3:
-				x++;
-				rotate.rotate(0, -90, 0);				
+				rotate.rotate(0, -90, 0);
 				break;
 		}
 		
