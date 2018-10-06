968879d35928c2e07546d1f65c4b14ca7d638299
James Horsley <jbhorsley@gmail.com>
2018-10-06 02:22:54 +0100

    Added Path model JAR folder from builds ignored

---
+++ src/org/jmc/models/Path.java
@@ -0,0 +1,31 @@
+package org.jmc.models;
+ 
+import org.jmc.geom.UV;
+import org.jmc.threading.ChunkProcessor;
+import org.jmc.threading.ThreadChunkDeligate;
+ 
+ 
+/**
+ * Model for path block.
+ */
+public class Path extends BlockModel
+{
+   
+    @Override
+    public void addModel(ChunkProcessor obj, ThreadChunkDeligate chunks, int x, int y, int z, byte data, int biome)
+    {
+        boolean[] drawSides = drawSides(chunks, x, y, z);
+       
+        UV[] uvSide = new UV[] { new UV(0,0), new UV(1,0), new UV(1,15/16f), new UV(0,15/16f) };
+        UV[][] uvSides = new UV[][] { null, uvSide, uvSide, uvSide, uvSide, null };
+ 
+        addBox(obj,
+                x-0.5f, y-0.5f, z-0.5f,
+                x+0.5f, y+0.4375f, z+0.5f,
+                null,
+                getMtlSides(data,biome),
+                uvSides,
+                drawSides);
+    }
+ 
+}
