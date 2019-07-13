af523fc3cfefa81c17680701ce13cf0ba024b82a
James Horsley <jbhorsley@gmail.com>
2019-07-13 03:12:45 +0100

    Fixed CubeFacing occlusion

--- src/org/jmc/models/CubeFacing.java
+++ src/org/jmc/models/CubeFacing.java
@@ -24,13 +24,67 @@ public class CubeFacing extends BlockModel
 		trans.translate(x, y, z);
 		rotate.rotate(dir);
 		
+		boolean[] dSides = drawSides(chunks, x, y, z);
+		boolean[] dSidesRot = new boolean[6];
+		//  or top, front, back, left, right, bottom
+		//    (top, north, south, west, east, bottom)
+		//      0     1      2     3     4      5
+		
+		switch (dir)
+		{
+			default:
+			case NORTH:
+				dSidesRot = dSides;
+				break;
+			case SOUTH:
+				dSidesRot[0] = dSides[0];
+				dSidesRot[1] = dSides[2];
+				dSidesRot[2] = dSides[1];
+				dSidesRot[3] = dSides[4];
+				dSidesRot[4] = dSides[3];
+				dSidesRot[5] = dSides[5];
+				break;
+			case EAST:
+				dSidesRot[0] = dSides[0];
+				dSidesRot[1] = dSides[4];
+				dSidesRot[2] = dSides[3];
+				dSidesRot[3] = dSides[1];
+				dSidesRot[4] = dSides[2];
+				dSidesRot[5] = dSides[5];
+				break;
+			case WEST:
+				dSidesRot[0] = dSides[0];
+				dSidesRot[1] = dSides[3];
+				dSidesRot[2] = dSides[4];
+				dSidesRot[3] = dSides[2];
+				dSidesRot[4] = dSides[1];
+				dSidesRot[5] = dSides[5];
+				break;
+			case UP:
+				dSidesRot[0] = dSides[2];
+				dSidesRot[1] = dSides[0];
+				dSidesRot[2] = dSides[5];
+				dSidesRot[3] = dSides[3];
+				dSidesRot[4] = dSides[4];
+				dSidesRot[5] = dSides[1];
+				break;
+			case DOWN:
+				dSidesRot[0] = dSides[1];
+				dSidesRot[1] = dSides[5];
+				dSidesRot[2] = dSides[0];
+				dSidesRot[3] = dSides[3];
+				dSidesRot[4] = dSides[4];
+				dSidesRot[5] = dSides[2];
+				break;
+		}
+		
 		addBox(obj,
 				-0.5f, -0.5f, -0.5f,
 				0.5f, 0.5f, 0.5f, 
 				trans.multiply(rotate), 
 				getMtlSides(data,biome), 
 				null, 
-				drawSides(chunks, x, y, z));
+				dSidesRot);
 	}
 
 }
