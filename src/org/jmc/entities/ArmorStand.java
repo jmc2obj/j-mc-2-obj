83667fd7d8e9baab915123b5a77ab5fd82138a83
James Horsley <jbhorsley@gmail.com>
2023-10-26 00:11:39 +0100

    Fix armor stand errors when wearing non armor items Fixes #234

--- src/org/jmc/entities/ArmorStand.java
+++ src/org/jmc/entities/ArmorStand.java
@@ -1,6 +1,9 @@
 package org.jmc.entities;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.annotation.Nonnull;
 
@@ -27,11 +30,8 @@ public class ArmorStand extends Entity {
 	}
 	
 	private Transform getTranslate(TAG_Compound entity, float x, float y, float z, float scale) {
-		TAG_List pos = (TAG_List) entity.getElement("Pos");
-		float ex=(float)((TAG_Double)pos.getElement(0)).value-0.5f + x;
-		float ey=(float)((TAG_Double)pos.getElement(1)).value-0.5f + y;
-		float ez=(float)((TAG_Double)pos.getElement(2)).value-0.5f + z;
-		Transform translate = Transform.translation(ex, ey, ez);
+		Vertex vert = Vertex.add(getPosition(entity), new Vertex(x, y, z));
+		Transform translate = Transform.translation(vert.x, vert.y, vert.z);
 		
 		Transform tScale = Transform.scale(scale, scale, scale);
 		
@@ -53,6 +53,9 @@ public class ArmorStand extends Entity {
 		return rotate;
 	}
 
+	private static final Set<String> armor_parts = new HashSet<>(
+			Arrays.asList("helmet", "chestplate", "leggings", "boots")
+	);
 	
 	@Override
 	public void addEntity(ChunkProcessor obj, TAG_Compound entity) {
@@ -64,49 +67,54 @@ public class ArmorStand extends Entity {
 		
 		for(int i=0; i<4; i++) {
 			TAG_Compound armor = ((TAG_Compound)Equipment.getElement(i));
-			String item_id = "";
-			try {
-				item_id = ((TAG_String)armor.getElement("id")).value;
-			} catch (NullPointerException e) {
-				Log.debug("Slot "+i+" not armed");
+			TAG_String item_id_tag = (TAG_String)armor.getElement("id");
+			if (item_id_tag == null) {
+				Log.debug("Armour stand "+getPosition(entity).toString()+" slot " + i + " not armed");
 				continue;
+			}
+			NamespaceID item_id = NamespaceID.fromString(item_id_tag.value);
+			String[] item_name_parts = item_id.path.split("_");
+			String armor_material = null;
+			if (item_name_parts.length == 2 && armor_parts.contains(item_name_parts[1])) {
+				armor_material = item_name_parts[0];
 			}
 			TAG_Compound tag = ((TAG_Compound)armor.getElement("tag"));
 			
 			TAG_List enchanted = null;
-			try {
-				enchanted = ((TAG_List)tag.getElement("ench"));
-			} catch (Exception e) {
+			if (tag != null) {
+				enchanted = ((TAG_List)tag.getElement("Enchantments"));
 			}
-			
-			String mcMaterial = item_id.substring(item_id.indexOf(":")+1, item_id.indexOf("_"));
 			
 			// internal: offset x, y, z, rotate, initialscale
 			// blender:  offset ?, z, -y, rotate, initialscale
-			double posCorrection[][] = {
+			double[][] posCorrection = {
 					{0, 0.194, -0.03, 0, 1}, // feet
 					{0, 0.778, 0.01, 0, 1}, // legs
 					{0, 1.14, 0.002, 180, 1}, // chest
 					{0, 1.71, 0, 180, 1} // helmet
 			};
 			
-			String tex[] = {
+			String[] tex = {
 					"layer_2", //feet
 					"layer_2", //legs
 					"layer_1", //chest
 					"layer_1"  //helmet
 			};
 			
-			String model[] = {
+			String[] model = {
 					"feet", //feet
 					"legs", //legs
 					"chest", //chest
 					"helmet"  //helmet
 			};
 			
-			if (mcMaterial.equals("golden")) mcMaterial = "gold";
-			if (mcMaterial.equals("player")) {
-				NBT_Tag skullOwnerNbt = tag.getElement("SkullOwner");
+			if (item_id.equals(new NamespaceID("minecraft", "player_head"))) {
+				NBT_Tag skullOwnerNbt;
+				if (tag != null) {
+					skullOwnerNbt = tag.getElement("SkullOwner");
+				} else {
+					skullOwnerNbt = new TAG_Compound("SkullOwner");
+				}
 				if (skullOwnerNbt instanceof TAG_Compound) {
 					Transform t = getTranslate(entity, 0, 1.64f, 0, 1).multiply(getRotate(entity, 180));
 					Head.addPlayerHead(obj, t, (TAG_Compound)skullOwnerNbt, null);
@@ -115,47 +123,43 @@ public class ArmorStand extends Entity {
 			}
 			
 			// base material
-			if (model[i] != null) {
+			if (armor_material != null) {
+				if (armor_material.equals("golden")) armor_material = "gold";
 				addArmor("conf/models/armor_"+model[i]+".obj", 
-						NamespaceID.fromString("models/armor/" + mcMaterial + "_"+tex[i]), 
+						NamespaceID.fromString("models/armor/" + armor_material + "_"+tex[i]),
 						obj, entity, 
 						posCorrection[i][0], 
 						posCorrection[i][1],
 						posCorrection[i][2],
 						1 * posCorrection[i][4],
 						posCorrection[i][3]);
-			}
-			
-			// leather overlay
-			if (mcMaterial.equals("leather")) {
-				if (model[i] != null) {
-					addArmor("conf/models/armor_"+model[i]+".obj", 
-							NamespaceID.fromString("models/armor/" + mcMaterial + "_"+tex[i]+"_overlay"), 
-							obj, entity, 
-							posCorrection[i][0], 
+				
+				// leather overlay
+				if (armor_material.equals("leather")) {
+					addArmor("conf/models/armor_"+model[i]+".obj",
+							NamespaceID.fromString("models/armor/" + armor_material + "_"+tex[i]+"_overlay"),
+							obj, entity,
+							posCorrection[i][0],
 							posCorrection[i][1],
 							posCorrection[i][2],
 							1.04 * posCorrection[i][4],
 							posCorrection[i][3]);
 				}
-			}
-			
-			// is enchanted
-			if (enchanted != null) {
-				if (model[i] != null) {
-					addArmor("conf/models/armor_"+model[i]+".obj", 
-							NamespaceID.fromString("misc/enchanted_item_glint"), 
-							obj, entity, 
-							posCorrection[i][0], 
+				
+				// is enchanted
+				if (enchanted != null) {
+					addArmor("conf/models/armor_"+model[i]+".obj",
+							NamespaceID.fromString("misc/enchanted_item_glint"),
+							obj, entity,
+							posCorrection[i][0],
 							posCorrection[i][1],
 							posCorrection[i][2],
 							1.08 * posCorrection[i][4],
 							posCorrection[i][3]);
 				}
 			}
-			 
+			
 		}
-		
 	}
 
 	public void addArmor(String objFileName, NamespaceID material, ChunkProcessor obj, TAG_Compound entity, double x, double y, double z, double scale, double rotation) {
