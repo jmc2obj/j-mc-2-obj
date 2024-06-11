1d215bc97d133483f74e1fc941e9f93ef1af2f93
Sunshine-Planet <sunshine.p.2002@gmail.com>
2024-06-11 12:09:25 +0800

    Fix missing class

--- src/org/jmc/util/Messages.java
+++ src/org/jmc/util/Messages.java
@@ -1,6 +1,7 @@
 package org.jmc.util;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.MissingResourceException;
